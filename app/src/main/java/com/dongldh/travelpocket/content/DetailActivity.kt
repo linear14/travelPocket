9e5b6620a52e8380cc33b190f0e3e998ee8a31d1
linear14 <47495592+linear14@users.noreply.github.com>
2020-03-21 21:34:35 +0900

    feat: DetailActivity layout 작성
    경비내역 클릭시 그 정보를 디테일하게 보여 줄 액티비티

---
+++ app/src/main/java/com/dongldh/travelpocket/content/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.dongldh.travelpocket.content
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongldh.travelpocket.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
