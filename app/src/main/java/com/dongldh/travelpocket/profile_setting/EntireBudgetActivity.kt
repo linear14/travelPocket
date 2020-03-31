0ece7a90f7bd0b1e3a97bb4b88d9e0d8ef405aaf
linear14 <47495592+linear14@users.noreply.github.com>
2020-03-31 22:45:22 +0900

    feat: 전체적인 예산/화폐 관리 레이아웃 완성

---
+++ app/src/main/java/com/dongldh/travelpocket/profile_setting/EntireBudgetActivity.kt
@@ -0,0 +1,14 @@
+package com.dongldh.travelpocket.profile_setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongldh.travelpocket.R
+
+class EntireBudgetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entire_budget)
+        title = "에산/화폐"
+    }
+}
